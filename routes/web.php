b5a6a135e3f2c93607eaa73c978f2492ef8e4f93
Matt Kenefick <keneficksays@gmail.com>
2024-08-27 20:51:57 -0400

    v3 prefix

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,7 @@ Route::get('/', function () {
 	return view('welcome');
 });
 
-
-Route::prefix('v1')->group(function () {
+Route::prefix('v3')->group(function () {
 	Route::get('/ads', [App\Http\Controllers\v1\Ads::class, 'getIndex']);
 	Route::get('/feed', [App\Http\Controllers\v1\Feed::class, 'getIndex']);
 
